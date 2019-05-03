8737b24f460fd4a20b4f71f05ae788c20c07d1bb
Lawlighty <49227867+Lawlighty@users.noreply.github.com>
2019-05-03 17:52:51 +0800

    Add files via upload

---
+++ 网络爬虫/本地区域房价/showPic.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+from Mean import MyMean
+import matplotlib.pyplot as plt
+
+yuechengData = pd.read_excel('越城二手房房.xlsx')['total_price']
+yuecprice = MyMean(yuechengData)
+
+keqiaoData = pd.read_excel('柯桥二手房.xlsx')['total_price']
+keqiaoprice = MyMean(keqiaoData)
+
+jinhuData = pd.read_excel('镜湖二手房.xlsx')['total_price']
+jinhuprice = MyMean(jinhuData)
+
+binhaiData = pd.read_excel('滨海二手房.xlsx')['total_price']
+binhaiprice = MyMean(binhaiData)
+
+paojiangData = pd.read_excel('袍江二手房.xlsx')['total_price']
+paojiangprice = MyMean(paojiangData)
+
+zhujiData = pd.read_excel('诸暨二手房.xlsx')['total_price']
+zhujiprice = MyMean(zhujiData)
+
+shangyuData = pd.read_excel('上虞二手房.xlsx')['total_price']
+shangyuprice = MyMean(shangyuData)
+
+shengzhouData = pd.read_excel('嵊州二手房.xlsx')['total_price']
+shengzhouprice = MyMean(shengzhouData)
+
+xinchangData = pd.read_excel('新昌二手房.xlsx')['total_price']
+xinchangprice = MyMean(xinchangData)
+
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus'] =False
+
+xlabel = ['越城','柯桥','镜湖','滨海','袍江','诸暨','上虞','嵊州','新昌']
+ylabel = [yuecprice, keqiaoprice, jinhuprice, binhaiprice, paojiangprice, zhujiprice, shangyuprice, shengzhouprice, xinchangprice ]
+plt.bar(xlabel, ylabel, color='cmyrgb')
+# color='c' 'm' 'y' 'k' 'r' 'g' 'b','tan','sage'
+plt.xlabel('区域')
+plt.ylabel('价格 元/M2')
+plt.title('绍兴市 二手房 价格', fontsize=16)
+plt.axis([-1,9,10000,15000])
+plt.show()
