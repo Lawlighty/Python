7376e9a8c6e963fa3b7f99cac92560cc74a14604
Lawlighty <49227867+Lawlighty@users.noreply.github.com>
2019-07-02 12:44:09 +0800

    Add files via upload

---
+++ 网络爬虫/爬虫框架Scrapy/gitLogin/gitLogin/spiders/git.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import logging
+
+class GitSpider(scrapy.Spider):
+    name = 'git'
+    allowed_domains = ['https://github.com/']
+    # start_urls = ['https://github.com/login']
+    def start_requests(self):
+        return [
+            scrapy.Request("https://github.com/login",
+                        meta={'cookiejar': 1}, callback=self.post_login)
+        ]
+
+    def post_login(self, response):
+        # 先去拿隐藏的表单参数authenticity_token
+        authenticity_token = response.xpath('//input[@name="authenticity_token"]/@value').extract_first()
+        logging.info('authenticity_token=' + authenticity_token)
+        return [
+            scrapy.FormRequest.from_response(
+                  response,
+                  url='https://github.com/session',
+                  meta={'cookiejar': response.meta['cookiejar']},
+                  formdata={
+                      'commit': 'Sign in',
+                      'utf8':'✓',
+                      'authenticity_token':authenticity_token,
+                      'login':'Lawlighty',
+                      'password':'liyixin123',
+                  },
+                  callback=self.git_ok,
+                  dont_filter=True
+                  )
+        ]
+
+    def git_ok(self, response):
+
+        try:
+            img = response.xpath('//img[@alt="@Lawlighty"]')
+            if(img):
+                print('登录成功')
+        except :
+            print('登录失败')
+
+
+
